8e674262cbf676b5492970dfe58bea01048ea821
Scott Lowe <scottlowe0@gmail.com>
2013-04-05 22:51:44 +0100

    Added javascript from http://www.html-form-guide.com/contact-form/simple-modal-popup-contact-form.html

--- BookCommunityQuestions.user.js
+++ BookCommunityQuestions.user.js
@@ -6,6 +6,10 @@
 // @include  http://read.amazon.com/
 // @run-at        document-end
 // @grant         none
+// @require fg_ajax.js
+// @require fg_form_submitter.js
+// @require fg_moveable_popup.js
+// @require gen_validatorv31.js
 // ==/UserScript==
 // Notes:
 //   * is a wildcard character
@@ -22,5 +26,13 @@ function Greasemonkey_main() {
 
 window.setTimeout(Greasemonkey_main, 2000);
 
+
+function loc = get_read_loc() {
+    var doc2=document.getElementById("KindleReaderIFrame").contentDocument;
+    var loct = doc2.getElementById('kindleReader_footer_message').textContent;
+    var patt=/% · (\d+) of/;
+    var matches = patt.exec(loct);
+    var loc = matches[1];
+}
 
 
