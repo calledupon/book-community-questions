88b007db7d4d4939916a612594c766346eb1e690
Miha Pelko <mpelko@gmail.com>
2013-04-05 23:52:18 +0100

    added the first communication functions

--- BookCommunityQuestions.user.js
+++ BookCommunityQuestions.user.js
@@ -6,33 +6,30 @@
 // @include  http://read.amazon.com/
 // @run-at        document-end
 // @grant         none
+// @require globals.js
 // @require fg_ajax.js
 // @require fg_form_submitter.js
 // @require fg_moveable_popup.js
 // @require gen_validatorv31.js
+// @require db_communication.js
 // ==/UserScript==
 // Notes:
 //   * is a wildcard character
 //   .tld is magic that matches all top-level domains (e.g. .com, .co.uk, .us, etc.)
 
-
-function Greasemonkey_main() {
-   var doc2=document.getElementById("KindleReaderIFrame").contentDocument;
-   var loct = doc2.getElementById('kindleReader_footer_message').textContent;
-   var patt=/% · (\d+) of/;
-   var match = patt.exec(loct);
-   alert(match[1]);
-}
-
-window.setTimeout(Greasemonkey_main, 2000);
-
-
-function loc = get_read_loc() {
+function get_read_loc() {
     var doc2=document.getElementById("KindleReaderIFrame").contentDocument;
     var loct = doc2.getElementById('kindleReader_footer_message').textContent;
     var patt=/% · (\d+) of/;
     var matches = patt.exec(loct);
     var loc = matches[1];
+    return loc;
 }
 
+function greasemonkey_main() {
+   loc = get_read_loc()
+   alert(loc);
+}
+
+setTimeout(greasemonkey_main, 4000);
 
